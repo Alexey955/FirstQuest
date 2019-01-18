58c1c0e68dbe855a153f0a280e82b9617536a50e
Aleksey <alekseyvasiljev@gmail.com>
2019-01-18 18:03:57 +0300

    Project is ready to package.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String filePath = "src/main/resources/FileInput.txt";
-
-        StatsCollector statsCollector = new StatsCollector(filePath);
+        StatsCollector statsCollector = new StatsCollector(args[0]);
         statsCollector.showStats();
     }
 }
